f9878beede4f3b704daf1c908d2dae92e0b926b1
ivan l <317868+iloire@users.noreply.github.com>
2023-01-21 20:09:55 +0000

    chore: fix grid dimensions

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -4,13 +4,13 @@ const Helpers = {
   createHelpers: (scene) => {
     const grid = new THREE.Object3D();
 
-    const gridH = new THREE.GridHelper(400, 10, 0x0000ff, 0x808080);
+    const gridH = new THREE.GridHelper(100, 10, 0x0000ff, 0x808080);
     gridH.position.y = 0;
     gridH.position.x = 0;
     gridH.rotation.x = 0;
     grid.add(gridH);
 
-    const gridV = new THREE.GridHelper(400, 10, 0x0000ff, 0x808080);
+    const gridV = new THREE.GridHelper(100, 10, 0x0000ff, 0x808080);
     gridV.position.y = 0;
     gridV.position.x = 0;
     gridV.rotation.x = -Math.PI / 2;
