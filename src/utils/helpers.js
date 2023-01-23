a127f4474fcd13388d97cc2728def7ea64474ab6
ivan l <317868+iloire@users.noreply.github.com>
2023-01-23 11:54:08 +0000

    chore: add arrowhelper for wind direction

--- src/utils/helpers.js
+++ src/utils/helpers.js
@@ -17,6 +17,17 @@ const Helpers = {
     grid.add(gridV);
 
     scene.add(grid);
+
+    const dir = new THREE.Vector3(10, 0, -2);
+    //normalize the direction vector (convert to vector of length 1)
+    dir.normalize();
+
+    const origin = new THREE.Vector3(-100, 10, 0);
+    const length = 100;
+    const hex = 0xffff00;
+
+    const arrowHelper = new THREE.ArrowHelper(dir, origin, length, hex);
+    scene.add(arrowHelper);
   },
 };
 export default Helpers;
