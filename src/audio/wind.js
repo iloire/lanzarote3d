bd19ca4d0c57d84f2b4645edcd12ec29ae4ba682
ivan l <317868+iloire@users.noreply.github.com>
2023-02-18 21:32:41 +0000

    feat: add background music

--- src/audio/wind.js
+++ src/audio/wind.js
@@ -1,23 +1,29 @@
 import * as THREE from "three";
 import wind1 from "./wind-howl-01.mp3";
+import wind2 from "./hurricane-01.mp3";
+import music from "./the-beat-of-nature-122841.mp3";
+
+const play = (camera, file) => {
+  const listener = new THREE.AudioListener();
+  camera.add(listener);
+
+  // create a global audio source
+  const sound = new THREE.Audio(listener);
+
+  // load a sound and set it as the Audio object's buffer
+  const audioLoader = new THREE.AudioLoader();
+  audioLoader.load(file, function (buffer) {
+    sound.setBuffer(buffer);
+    sound.setLoop(true);
+    sound.setVolume(0.3);
+    sound.play();
+  });
+};
 
 const Wind = {
   load: (camera) => {
-    const listener = new THREE.AudioListener();
-    camera.add(listener);
-
-    // create a global audio source
-    const sound = new THREE.Audio(listener);
-
-    // load a sound and set it as the Audio object's buffer
-    const audioLoader = new THREE.AudioLoader();
-
-    audioLoader.load(wind1, function (buffer) {
-      sound.setBuffer(buffer);
-      sound.setLoop(true);
-      sound.setVolume(0.2);
-      sound.play();
-    });
+    play(camera, wind1);
+    play(camera, music);
   },
 };
 
