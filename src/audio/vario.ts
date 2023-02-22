a81b43c9db95cc45586817c29bd6577cd9efd1b6
ivan l <317868+iloire@users.noreply.github.com>
2023-02-22 09:16:15 +0000

    feat: add proper sounds for vario

--- src/audio/vario.ts
+++ src/audio/vario.ts
@@ -1,6 +1,9 @@
 import beep200 from "./beep/audiocheck.net_sin_200Hz_-3dBFS_0.3s.wav";
+import beep400 from "./beep/audiocheck.net_sin_400Hz_-3dBFS_0.3s.wav";
+import beep600 from "./beep/audiocheck.net_sin_600Hz_-3dBFS_0.3s.wav";
 import beep800 from "./beep/audiocheck.net_sin_800Hz_-3dBFS_0.3s.wav";
 import beep1000 from "./beep/audiocheck.net_sin_1000Hz_-3dBFS_0.3s.wav";
+import beep1200 from "./beep/audiocheck.net_sin_1200Hz_-3dBFS_0.3s.wav";
 import * as THREE from "three";
 import Paraglider from "../elements/pg";
 
@@ -39,13 +42,30 @@ class Vario extends THREE.EventDispatcher {
     this.dispatchEvent({ type: "delta", delta });
     this.lastRecord = this.high;
     if (this.status === "on") {
-      // this.play(1);
+      this.play(delta);
     }
   };
 
-  play(increment) {
+  getBeepForIncrement(delta) {
+    if (delta > 2) {
+      return beep1200;
+    } else if (delta > 1.5) {
+      return beep1000;
+    } else if (delta > 1) {
+      return beep800;
+    } else if (delta > 0) {
+      return beep600;
+    } else if (delta > -0.5) {
+      return beep400;
+    } else {
+      return beep200;
+    }
+  }
+
+  play(delta) {
+    console.log(delta);
     const audioLoader = new THREE.AudioLoader();
-    audioLoader.load(beep1000, (buffer) => {
+    audioLoader.load(this.getBeepForIncrement(delta), (buffer) => {
       this.sound.setBuffer(buffer);
       this.sound.setVolume(0.01);
       this.sound.play();
