291431e1ec49dc9afbabc8c50f059378a4c90a1e
ivan l <317868+iloire@users.noreply.github.com>
2023-02-26 18:56:52 +0100

    chore: refactor thermal

--- src/elements/thermal.ts
+++ src/elements/thermal.ts
@@ -1,6 +1,31 @@
 import * as THREE from "three";
 import Weather from "./weather";
 
+const createThermalMesh = (
+  bottomRadius: number,
+  topRadius: number,
+  height: number,
+  initialPosition: THREE.Vector3
+) => {
+  const geometry = new THREE.CylinderGeometry(
+    topRadius,
+    bottomRadius,
+    height,
+    32
+  );
+
+  const material = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    opacity: 0.05, // set the opacity level (0-1)
+    transparent: true, // enable transparencyopacity: 0.5, // set the opacity level (0-1)
+  });
+
+  const cylinder = new THREE.Mesh(geometry, material);
+  initialPosition.y = initialPosition.y + height / 2;
+  cylinder.position.copy(initialPosition);
+
+  return cylinder;
+};
 class Thermal {
   mesh: THREE.Mesh;
 
@@ -10,27 +35,13 @@ class Thermal {
     weather: Weather,
     relativeScale: number = 1
   ) {
-    const geometry = new THREE.CylinderGeometry(
+    const thermal = createThermalMesh(
       250 * relativeScale,
-      100 * relativeScale,
+      150 * relativeScale,
       height,
-      32
+      initialPosition
     );
-
-    const material = new THREE.MeshBasicMaterial({
-      color: 0xff0000,
-      opacity: 0.05, // set the opacity level (0-1)
-      transparent: true, // enable transparencyopacity: 0.5, // set the opacity level (0-1)
-    });
-
-    const cylinder = new THREE.Mesh(geometry, material);
-    initialPosition.y = initialPosition.y + height / 2;
-    cylinder.position.copy(initialPosition);
-
-    // const vectorWind = weather.getWindDirection().clone();
-    // cylinder.lookAt(new THREE.Vector3(0, 0, -1));
-
-    this.mesh = cylinder;
+    this.mesh = thermal;
   }
 
   getMesh(): THREE.Mesh {
