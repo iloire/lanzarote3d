f91a95186e6638230516bcb7c81350b9f10de7b3
ivan l <317868+iloire@users.noreply.github.com>
2023-02-27 11:53:42 +0100

    chore: return to orbit mode on game over

--- src/stories/game.tsx
+++ src/stories/game.tsx
@@ -234,6 +234,7 @@ const Game = {
       console.log(pg.getTrajectory());
       const trajectory = new Trajectory(pg.getTrajectory(), 10);
       scene.add(trajectory.getMesh());
+      camera.setCameraMode(CameraMode.OrbitControl, pg, controls);
     });
 
     const animate = () => {
