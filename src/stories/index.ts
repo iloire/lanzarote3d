d047c04d2ca9ac8d80b27df9a0726910f4d55969
ivan l <317868+iloire@users.noreply.github.com>
2023-02-27 11:15:25 +0100

    chore: remove balloons

--- src/stories/index.ts
+++ src/stories/index.ts
@@ -1,12 +1,10 @@
 import Default from "./default";
-import Balloons from "./balloons";
 import FlyZones from "./flyzones";
 import Game from "./game";
 import Mechanics from "./mechanics";
 
 const Stories = {
   default: Default.load,
-  balloons: Balloons.load,
   flyzones: FlyZones.load,
   game: Game.load,
   mechanics: Mechanics.load,
