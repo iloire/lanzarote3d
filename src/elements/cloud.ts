05e2bde57610b2f5685c2e5660ce6ee8473e2f67
ivan l <317868+iloire@users.noreply.github.com>
2023-03-01 01:06:32 +0100

    chore: cleanup cloud and fix oscilation

--- src/elements/cloud.ts
+++ src/elements/cloud.ts
@@ -1,7 +1,6 @@
 import * as THREE from "three";
 import blackCloud from "../models/clouds.glb";
 import whiteCloud from "../models/low_poly_cloud.glb";
-import textureImg from "../textures/Parachute_01_D.png";
 import Models from "../utils/models";
 
 export type CloudType = "BLACK" | "WHITE";
@@ -9,9 +8,8 @@ export type CloudType = "BLACK" | "WHITE";
 const Cloud = {
   load: async (type: CloudType) => {
     const animate = (mesh) => {
-      const timer = Date.now() * 0.05;
-      // mesh.position.x = mesh.position.x + 0.3;
-      mesh.position.y = mesh.position.y + Math.sin(timer) * 0.1;
+      const timer = (Date.now() + Math.random() * 1000) * 0.001;
+      mesh.position.y = mesh.position.y + Math.sin(timer) * 1;
       requestAnimationFrame(() => animate(mesh));
     };
 
@@ -19,18 +17,6 @@ const Cloud = {
       type === "BLACK" ? blackCloud : whiteCloud,
       type === "BLACK" ? 2 : 30
     );
-
-    var mat001 = new THREE.MeshPhysicalMaterial();
-    mat001.color = new THREE.Color("gold");
-    mat001.reflectivity = 1.0;
-    mat001.roughness = 0.0;
-    mat001.envMapIntensity = 1.0;
-
-    // const textureLoader = new THREE.TextureLoader(Models.manager);
-    // const texture = await textureLoader.load(textureImg);
-    // mesh.material = new THREE.MeshStandardMaterial({ map: texture });
-    mesh.material = mat001;
-
     animate(mesh);
     return mesh;
   },
