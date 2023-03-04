c9ddedf096813a6fc2b10a5f5cb0d4ee2de099b9
ivan l <317868+iloire@users.noreply.github.com>
2023-03-04 14:07:20 +0000

    chore: remove trajectory from mechanics scene

--- src/stories/mechanics.ts
+++ src/stories/mechanics.ts
@@ -67,16 +67,6 @@ const Mechanics = {
     birdsMesh.position.copy(birdsPosition);
     scene.add(birdsMesh);
 
-    const points = [
-      new THREE.Vector3(29000, 1000, 3100),
-      new THREE.Vector3(3500, 800, 3200),
-      new THREE.Vector3(2300, 1200, 3300),
-      new THREE.Vector3(3000, 700, 3400),
-      new THREE.Vector3(5000, 1300, 3300),
-    ];
-    const trajectory = new Trajectory(points);
-    scene.add(trajectory.getMesh());
-
     console.log("Number of Triangles :", renderer.info.render.triangles);
     camera.position.set(4200, 2500, -3200);
 
