ef5069bc60111e70c97425d03c81c8be766e9660
ivan l <317868+iloire@users.noreply.github.com>
2023-03-08 13:04:45 +0000

    chore: remove low power webgl setting

--- src/index.tsx
+++ src/index.tsx
@@ -26,7 +26,7 @@ const gui = new GUI();
 
 const createRenderer = (sizes) => {
   const renderer = new THREE.WebGLRenderer({
-    powerPreference: "low-power" /* This can also be 'high-performance' */,
+    // powerPreference: "low-power" /* This can also be 'high-performance' */,
     canvas: document.querySelector("canvas.webgl"),
     antialias: true,
   });
