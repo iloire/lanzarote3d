9da50a950702d448faa1b41db1dca11f0eaf8d29
ivan l <317868+iloire@users.noreply.github.com>
2023-03-16 20:50:22 +0000

    fix: fix defaults and refactor

--- src/utils/gui.ts
+++ src/utils/gui.ts
@@ -1,41 +1,57 @@
+import * as THREE from "three";
+
 export type GuiHelperOptions = {
   max: number;
   min: number;
 };
 
 const GuiHelper = {
+  addRotationGui: (
+    gui,
+    name: string,
+    rotation: THREE.Euler,
+    options: GuiHelperOptions = { min: -Math.PI, max: Math.PI }
+  ) => {
+    const { min, max } = options;
+    const g = gui.addFolder(name);
+    g.add(rotation, "x", min, max)
+      .name(name + ".rotation.x")
+      .listen();
+    g.add(rotation, "y", min, max)
+      .name(name + ".rotation.y")
+      .listen();
+    g.add(rotation, "z", min, max)
+      .name(name + ".rotation.z")
+      .listen();
+  },
+
+  addPositionGui: (
+    gui,
+    name: string,
+    pos: THREE.Vector3,
+    options: GuiHelperOptions = { min: -20, max: 20 }
+  ) => {
+    const { min, max } = options;
+    const g = gui.addFolder(name);
+    g.add(pos, "x", min, max)
+      .name(name + ".position.x")
+      .listen();
+    g.add(pos, "y", min, max)
+      .name(name + ".position.y")
+      .listen();
+    g.add(pos, "z", min, max)
+      .name(name + ".position.z")
+      .listen();
+  },
+
   addLocationGui: (
     gui,
     name: string,
     obj: any,
     options: GuiHelperOptions = { min: -20, max: 20 }
   ) => {
-    const { min, max } = options;
-    const pgGui = gui.addFolder(name);
-    pgGui
-      .add(obj.position, "x", min, max)
-      .name(name + ".position.x")
-      .listen();
-    pgGui
-      .add(obj.position, "y", min, max)
-      .name(name + ".position.y")
-      .listen();
-    pgGui
-      .add(obj.position, "z", min, max)
-      .name(name + ".position.z")
-      .listen();
-    pgGui
-      .add(obj.rotation, "x", -Math.PI, Math.PI)
-      .name(name + ".rotation.x")
-      .listen();
-    pgGui
-      .add(obj.rotation, "y", -Math.PI, Math.PI)
-      .name(name + ".rotation.y")
-      .listen();
-    pgGui
-      .add(obj.rotation, "z", -Math.PI, Math.PI)
-      .name(name + ".rotation.z")
-      .listen();
+    GuiHelper.addPositionGui(gui, name, obj.position, options);
+    GuiHelper.addRotationGui(gui, name, obj.rotation);
   },
 };
 
