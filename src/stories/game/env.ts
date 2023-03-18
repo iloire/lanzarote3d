93050d568dbd265b799d4741974f3e381e1c37b3
ivan l <317868+iloire@users.noreply.github.com>
2023-03-18 11:04:36 +0000

    chore: env generation: better thermals, trees, tones

--- src/stories/game/env.ts
+++ src/stories/game/env.ts
@@ -2,19 +2,45 @@ import * as THREE from "three";
 import Clouds from "../../elements/clouds";
 import Weather from "../../elements/weather";
 import Thermal, { ThermalDimensions } from "../../elements/thermal";
+import { rndIntBetween } from "../../utils/math";
+import Tree from "../../components/tree";
+import Stone from "../../components/stone";
+
+const generateRandomLcl = (lclLevel: number): number => {
+  return lclLevel + rndIntBetween(-200, 3000);
+};
+
+const generateRandomThermalDimensions = (
+  lclLevel: number
+): ThermalDimensions => {
+  return {
+    bottomRadius: rndIntBetween(220, 350),
+    topRadius: rndIntBetween(400, 600),
+    height: generateRandomLcl(lclLevel),
+  };
+};
 
 const generateThermalPair = (
-  dimensions: ThermalDimensions,
   position: THREE.Vector3,
-  weather: Weather
+  weather: Weather,
+  dimensions?: ThermalDimensions
 ): Thermal[] => {
-  const thermal = new Thermal(dimensions, position, 0.05, weather, true);
+  const thermal = new Thermal(
+    dimensions || generateRandomThermalDimensions(weather.getLclLevel()),
+    position,
+    0.05,
+    weather,
+    true
+  );
+
+  const interiorThermalDimensions = {
+    bottomRadius: rndIntBetween(190, 250) / 2,
+    topRadius: rndIntBetween(400, 600) / 2,
+    height: generateRandomLcl(weather.getLclLevel()),
+  };
+
   const thermalInside = new Thermal(
-    {
-      bottomRadius: dimensions.bottomRadius / 2,
-      topRadius: dimensions.topRadius / 2,
-      height: dimensions.height,
-    },
+    interiorThermalDimensions,
     position,
     0.09,
     weather,
@@ -23,58 +49,116 @@ const generateThermalPair = (
   return [thermal, thermalInside];
 };
 
+const getTerrainHeight = (pos: THREE.Vector3, terrain: THREE.Mesh) => {
+  const rayVertical = new THREE.Raycaster(
+    new THREE.Vector3(pos.x, 10000, pos.z), // big enough value for Y
+    new THREE.Vector3(0, -1, 0) // vertical
+  );
+  const intersects = rayVertical.intersectObject(terrain);
+  if (intersects.length === 1) {
+    return intersects[0].point.y;
+  } else {
+    return NaN;
+  }
+};
+
+const addTreesAroundArea = (
+  pos: THREE.Vector3,
+  numberTrees: number,
+  terrain: THREE.Mesh,
+  scene: THREE.Scene
+) => {
+  for (let index = 0; index < numberTrees; index++) {
+    const newX = pos.x + 10 * index * rndIntBetween(0, 20);
+    const newZ = pos.z + 10 * index * rndIntBetween(0, 10);
+
+    const terrainHeight = getTerrainHeight(
+      new THREE.Vector3(newX, 0, newZ),
+      terrain
+    );
+    if (isNaN(terrainHeight)) {
+      break;
+    }
+    const treePos = new THREE.Vector3(newX, terrainHeight, newZ);
+    console.log(treePos);
+
+    const tree = new Tree().load();
+    tree.position.copy(treePos);
+    const scale = 5;
+    tree.scale.set(scale, scale, scale);
+    scene.add(tree);
+  }
+};
+
+const addMeshAroundArea = (
+  mesh: THREE.Mesh,
+  pos: THREE.Vector3,
+  numberTrees: number,
+  terrain: THREE.Mesh,
+  scene: THREE.Scene
+) => {
+  for (let index = 0; index < numberTrees; index++) {
+    const newX = pos.x + 10 * index * rndIntBetween(0, 20);
+    const newZ = pos.z + 10 * index * rndIntBetween(0, 10);
+
+    const terrainHeight = getTerrainHeight(
+      new THREE.Vector3(newX, 0, newZ),
+      terrain
+    );
+    if (isNaN(terrainHeight)) {
+      break;
+    }
+    const treePos = new THREE.Vector3(newX, terrainHeight, newZ);
+
+    const meshClone = mesh.clone();
+    meshClone.position.copy(treePos);
+    const scale = 5;
+    meshClone.scale.set(scale, scale, scale);
+    scene.add(meshClone);
+  }
+};
+
 const Environment = {
+  addStones: (scene: THREE.Scene, terrain: THREE.Mesh) => {
+    const stone = new Stone().load();
+    stone.scale.set(100, 100, 100);
+    addMeshAroundArea(stone, new THREE.Vector3(0, 600, 0), 100, terrain, scene);
+  },
+  addTrees: (scene: THREE.Scene, terrain: THREE.Mesh) => {
+    addTreesAroundArea(new THREE.Vector3(0, 600, 0), 100, terrain, scene);
+    addTreesAroundArea(new THREE.Vector3(6879, 600, -545), 100, terrain, scene);
+  },
   addThermals: (scene: THREE.Scene, weather: Weather): Thermal[] => {
     const lclLevel = weather.getLclLevel();
     // famara
-    const t1 = generateThermalPair(
-      { bottomRadius: 360, topRadius: 160, height: lclLevel },
-      new THREE.Vector3(5727, 0, -535),
-      weather
-    );
-    const t2 = generateThermalPair(
-      { bottomRadius: 360, topRadius: 160, height: lclLevel },
-      new THREE.Vector3(7127, 0, -1405),
-      weather
-    );
-    const t3 = generateThermalPair(
-      { bottomRadius: 360, topRadius: 160, height: lclLevel },
-      new THREE.Vector3(3027, 0, 1005),
-      weather
-    );
+    const t1 = generateThermalPair(new THREE.Vector3(5727, 0, -535), weather);
+    const t2 = generateThermalPair(new THREE.Vector3(7127, 0, -1405), weather);
+    const t3 = generateThermalPair(new THREE.Vector3(3027, 0, 1005), weather);
 
     // tenesar
-    const t4 = generateThermalPair(
-      { bottomRadius: 360, topRadius: 160, height: lclLevel },
-      new THREE.Vector3(-4827, 0, -855),
-      weather
-    );
+    const t4 = generateThermalPair(new THREE.Vector3(-4827, 0, -855), weather);
     // mirador
     const t5 = generateThermalPair(
-      { bottomRadius: 360, topRadius: 160, height: lclLevel },
       new THREE.Vector3(15027, 0, -12555),
       weather
     );
 
     // pq
-    const t6 = generateThermalPair(
-      { bottomRadius: 360, topRadius: 160, height: lclLevel },
-      new THREE.Vector3(-6227, 0, 14055),
-      weather
-    );
+    const t6 = generateThermalPair(new THREE.Vector3(-6227, 0, 14055), weather);
     // mala
-    const t7 = generateThermalPair(
-      { bottomRadius: 360, topRadius: 160, height: lclLevel },
-      new THREE.Vector3(14227, 0, -3755),
-      weather
-    );
+    const t7 = generateThermalPair(new THREE.Vector3(14227, 0, -3755), weather);
+
+    // close to pq
+    const t8 = generateThermalPair(new THREE.Vector3(-3927, 0, 9830), weather);
+
     const allThermals = t1
       .concat(t2)
       .concat(t3)
       .concat(t4)
       .concat(t5)
       .concat(t6)
-      .concat(t7);
+      .concat(t7)
+      .concat(t8);
     allThermals.forEach((t) => {
       scene.add(t.getMesh());
     });
@@ -94,7 +178,7 @@ const Environment = {
           1,
           new THREE.Vector3(
             t.getPosition().x,
-            lclLevel * 1.2,
+            t.getDimensions().height * (1 + 0.05 * rndIntBetween(1, 5)),
             t.getPosition().z
           )
         );
