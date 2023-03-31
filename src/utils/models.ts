70aeaca701b44dcafea2cefefc79aecd6f0124ca
ivan l <317868+iloire@users.noreply.github.com>
2023-03-31 11:13:37 +0100

    chore: add draco loader test

--- src/utils/models.ts
+++ src/utils/models.ts
@@ -1,11 +1,17 @@
 import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { DRACOLoader } from "three/examples/jsm/loaders/DRACOLoader";
 import * as THREE from "three";
 
 const defaultManager = new THREE.LoadingManager();
 
-function modelLoader(url, manager?: THREE.LoadingManager) {
-  console.log("loading model loader", url, manager);
-  const loader = new GLTFLoader(manager || defaultManager);
+function modelLoader(url, draco: boolean, manager?: THREE.LoadingManager) {
+  const man = manager || defaultManager;
+  const loader = draco ? new DRACOLoader(man) : new GLTFLoader(man);
+  console.log(loader);
+  if (draco) {
+    // loader.setDecoderPath("/examples/jsm/libs/draco/");
+    // loader.preload();
+  }
   return new Promise((resolve, reject) => {
     loader.load(url, (data) => resolve(data), null, reject);
   });
@@ -14,10 +20,17 @@ function modelLoader(url, manager?: THREE.LoadingManager) {
 const Models = {
   manager: defaultManager,
   loadGltf: async (model: string, manager?: THREE.LoadingManager) => {
-    return modelLoader(model, manager);
+    return modelLoader(model, false, manager);
+  },
+  loadSimpleDraco: async (model: string, manager?: THREE.LoadingManager) => {
+    const gltf: any = await modelLoader(model, true, manager);
+    const mesh =
+      gltf.scene.children.length === 1 ? gltf.scene.children[0] : gltf.scene;
+    mesh.castShadow = true;
+    return mesh;
   },
   loadSimple: async (model: string, manager?: THREE.LoadingManager) => {
-    const gltf: any = await modelLoader(model, manager);
+    const gltf: any = await modelLoader(model, false, manager);
     const mesh =
       gltf.scene.children.length === 1 ? gltf.scene.children[0] : gltf.scene;
     mesh.castShadow = true;
@@ -25,7 +38,7 @@ const Models = {
   },
   load: async (model: THREE.Mesh, scale: number, pos?: THREE.Vector3) => {
     console.warn("deprecated, use loadsimple instead");
-    const gltf: any = await modelLoader(model);
+    const gltf: any = await modelLoader(model, false);
     const mesh = gltf.scene.children[0];
     mesh.scale.set(scale, scale, scale);
     if (pos) {
