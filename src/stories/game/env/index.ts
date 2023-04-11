d01eef6d61749e8c280c8023581727edb005c29f
ivan l <317868+iloire@users.noreply.github.com>
2023-04-11 12:59:37 +0100

    chore: conditionally add birds or hg to env

--- src/stories/game/env/index.ts
+++ src/stories/game/env/index.ts
@@ -3,6 +3,9 @@ import Thermal from "../../../components/thermal";
 import Environment from "../env/environment";
 import Weather from "../../../elements/weather";
 
+const BIRDS = false;
+const HGLIDER = false;
+
 export const addGameEnvironment = (
   scene: THREE.Scene,
   terrain: THREE.Mesh,
@@ -19,27 +22,34 @@ export const addGameEnvironment = (
   env.addStones(terrain);
   env.addHouses(terrain);
   env.addBoats(water);
-  const birdsPath = [
-    { x: 7500, y: 1090, z: -1068 },
-    { x: 6500, y: 1190, z: -1368 },
-    { x: 4500, y: 1390, z: -1768 },
-  ];
-  env.addBirds(
-    birdsPath.map((p) => new THREE.Vector3(p.x, p.y, p.z)),
-    gui
-  );
-  const hgPath = [
-    { x: 10000, y: 1090, z: -6068 },
-    { x: 6500, y: 1190, z: -1368 },
-    { x: 8200, y: 1190, z: -1668 },
-    { x: 8900, y: 1390, z: -2768 },
-    { x: 9500, y: 1790, z: -4268 },
-    { x: 11000, y: 2790, z: -7468 },
-  ];
-  env.addHangGlider(
-    hgPath.map((p) => new THREE.Vector3(p.x, p.y, p.z)),
-    gui
-  );
+
+  if (BIRDS) {
+    const birdsPath = [
+      { x: 7500, y: 1090, z: -1068 },
+      { x: 6500, y: 1190, z: -1368 },
+      { x: 4500, y: 1390, z: -1768 },
+    ];
+    env.addBirds(
+      birdsPath.map((p) => new THREE.Vector3(p.x, p.y, p.z)),
+      gui
+    );
+  }
+
+  if (HGLIDER) {
+    const hgPath = [
+      { x: 10000, y: 1090, z: -6068 },
+      { x: 6500, y: 1190, z: -1368 },
+      { x: 8200, y: 1190, z: -1668 },
+      { x: 8900, y: 1390, z: -2768 },
+      { x: 9500, y: 1790, z: -4268 },
+      { x: 11000, y: 2790, z: -7468 },
+    ];
+    env.addHangGlider(
+      hgPath.map((p) => new THREE.Vector3(p.x, p.y, p.z)),
+      gui
+    );
+  }
+
   // await env.addOtherGliders(scene, weather, terrain, water);
   //
   return env;
