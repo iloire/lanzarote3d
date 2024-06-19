9c81a44eea63c77922f085bc896bebc8dfb0f78c
ivan l <317868+iloire@users.noreply.github.com>
2024-06-20 01:03:44 +0200

    chore: harness tweaks

--- src/components/parts/cocoon-harness.ts
+++ src/components/parts/cocoon-harness.ts
@@ -12,6 +12,7 @@ export type CocoonHarnessOptions = {
   color2: string;
 }
 
+
 class CocoonHarness {
   options: CocoonHarnessOptions;
 
@@ -20,10 +21,20 @@ class CocoonHarness {
   }
 
   load(): THREE.Object3D {
+    const getSquare = (w, h, z, posX, posY, posZ) => {
+      const suitMat = getColoredMaterial(this.options.color1 || '#333');
+      const geo = new THREE.BoxGeometry(w, h, z);
+      const mesh = new THREE.Mesh(geo, suitMat);
+      mesh.position.x = posX;
+      mesh.position.y = posY;
+      mesh.position.z = posZ;
+      return mesh;
+    }
+
     const suitMat = getColoredMaterial(this.options.color1 || '#333');
 
     const group = new THREE.Group();
-    const bodyGeo = new THREE.BoxGeometry(250, 420, 1200);
+    const bodyGeo = new THREE.BoxGeometry(250, 420, 800);
     const body = new THREE.Mesh(bodyGeo, suitMat);
     body.position.x = 0;
     body.position.y = -390;
@@ -37,6 +48,20 @@ class CocoonHarness {
     subBody.position.y = -385;
     subBody.position.z = 400;
     group.add(subBody);
+
+    const cola = getSquare(200, 300, 400, 0, -300, -400);
+    group.add(cola)
+
+    const front = getSquare(200, 300, 400, 0, -400, 800);
+    group.add(front)
+
+    const reserveHandleMat = getColoredMaterial('red');
+    const reserveHandleGeo = new THREE.BoxGeometry(90, 42, 130);
+    const reserve = new THREE.Mesh(reserveHandleGeo, reserveHandleMat);
+    reserve.position.x = -110;
+    reserve.position.y = -295;
+    reserve.position.z = 300;
+    group.add(reserve);
 
     return group;
   }
