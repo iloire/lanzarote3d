c65e6ab9637a280e70080d050d4a514895a32d07
ivan l <317868+iloire@users.noreply.github.com>
2024-06-25 23:44:14 +0200

    chore: extract getDefaultGlasses

--- src/components/parts/pilot-head.ts
+++ src/components/parts/pilot-head.ts
@@ -1,4 +1,5 @@
 import * as THREE from "three";
+import getDefaultGlasses from "./glasses/default";
 
 export enum PilotHeadType {
   Default, Warrior
@@ -191,53 +192,6 @@ const getDefaultHelmet = (options: PilotHeadOptions): THREE.Group => {
 }
 
 
-const getDefaultGlasses = (options: PilotHeadOptions): THREE.Group => {
-  const group = new THREE.Group();
-  //glasses
-  const glassGeo = new THREE.BoxGeometry(120, 78, 10);
-
-  //Retinas Left
-  const eyeMat = getColoredMaterial(options.eyeColor);
-  const glassLeft = new THREE.Mesh(glassGeo, eyeMat);
-  glassLeft.position.x = -80;
-  glassLeft.position.y = 4;
-  glassLeft.position.z = 160;
-
-  //Retinas Right
-  const glassRight = new THREE.Mesh(glassGeo, eyeMat);
-  glassRight.position.x = 80;
-  glassRight.position.y = 4;
-  glassRight.position.z = 160;
-  group.add(glassLeft);
-  group.add(glassRight);
-
-  //glass middle
-  const glassesMat = getColoredMaterial(options.glassesColor);
-  const glassMiddleGeo = new THREE.BoxGeometry(40, 10, 10);
-  const glassu = new THREE.Mesh(glassMiddleGeo, glassesMat);
-  glassu.position.x = 0;
-  glassu.position.y = 5;
-  glassu.position.z = 155;
-  group.add(glassu);
-
-  //Retinas
-  const retina = new THREE.BoxGeometry(25, 25, 5);
-  //Retinas Left
-  const pupilaMat = getColoredMaterial('#333');
-  const retinaLeft = new THREE.Mesh(retina, pupilaMat);
-  retinaLeft.position.x = -80;
-  retinaLeft.position.y = 5;
-  retinaLeft.position.z = 168;
-  //Retinas Right
-  const retinaRight = new THREE.Mesh(retina, pupilaMat);
-  retinaRight.position.x = 80;
-  retinaRight.position.y = 5;
-  retinaRight.position.z = 168;
-  group.add(retinaLeft);
-  group.add(retinaRight);
-
-  return group;
-}
 
 const getSunGlasses1 = (options: PilotHeadOptions): THREE.Group => {
   const group = new THREE.Group();
