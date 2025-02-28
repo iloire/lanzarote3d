a3964e6b30c0591395ad81286f18dcaeafe09d4f
ivan l <317868+iloire@users.noreply.github.com>
2025-02-28 16:42:56 +0000

    fix: fix story param from querystring

--- src/apps/main/index.tsx
+++ src/apps/main/index.tsx
@@ -8,10 +8,14 @@ import "../../index.css";
 
 THREE.Cache.enabled = true;
 
+const queryString = window.location.search;
+const urlParams = new URLSearchParams(queryString);
+const storyParam = urlParams.get("story") || "animation";
+
 const rootElement = document.getElementById("root");
 if (WebGL.isWebGLAvailable()) {
   const root = createRoot(rootElement);
-  root.render(<App initialStory="animation"  showAppSelection={true}/>);
+  root.render(<App initialStory={storyParam}  showAppSelection={true}/>);
 } else {
   const warning = WebGL.getWebGLErrorMessage();
   rootElement.appendChild(warning);
