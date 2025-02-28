7eae6d8d743407fe47b18acd0764ff991990312b
ivan l <317868+iloire@users.noreply.github.com>
2025-02-28 16:18:08 +0000

    chore: rename app

--- src/apps/animation1/index.tsx
+++ src/apps/animation1/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import { createRoot } from "react-dom/client";
 import * as THREE from "three";
 import WebGL from "../../WebGL";
-import { App } from "../../index";
+import { App } from "../../app";
 
 import "../../index.css";
 
