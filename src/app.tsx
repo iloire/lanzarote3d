cc3a77329e5c94f20783fc6abda08cc3842f67d5
ivan l <317868+iloire@users.noreply.github.com>
2025-03-23 13:42:54 +0000

    chore: make sure animation is loaded if no story is provided

--- src/app.tsx
+++ src/app.tsx
@@ -136,7 +136,7 @@ const App: React.FC<AppProps> = ({ initialStory, showAppSelection: initialShowAp
       controls
     };
 
-    await Stories[initialStory](storyOptions);
+    await Stories[initialStory] ? Stories[initialStory](storyOptions) : Stories.animation(storyOptions);
 
     // Animation loop
     const animate = () => {
