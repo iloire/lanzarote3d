c3d4bedb5bf43bcf6bff2f29bfac5c0d61c54dc8
ivan l. <317868+iloire@users.noreply.github.com>
2025-06-01 21:13:16 +0100

    chore: fix camera on flyzone initial position

--- src/stories/flyzones/index.tsx
+++ src/stories/flyzones/index.tsx
@@ -349,17 +349,12 @@ const FlyZones = {
     });
     
     // Set initial position
-    const initialPosition = locations.length > 0 
-      ? locations[0].position.clone() 
-      : new THREE.Vector3(14000, 8000, 14000);
+    const initialPosition =  new THREE.Vector3(14000, 8000, 14000);
   
-      
-      
-    navigateTo(initialPosition, camera, controls, locations.length > 0 ? locations[0] : undefined);
+    navigateTo(initialPosition, camera, controls, locations[0]);
     
     // Start animation loop
     setupAnimationLoop(renderer, scene, camera, controls, labelRenderer, markers, landingMarkersVisible);
-    
     
     // Setup mouse click handler
     const handleMouseClick = (event: MouseEvent) => {
--- src/stories/flyzones/navigation/camera.ts
+++ src/stories/flyzones/navigation/camera.ts
@@ -8,66 +8,7 @@ export const navigateTo = (
   position: THREE.Vector3, 
   camera: Camera,
   controls: OrbitControls,
-  location?: Location
-): void => {
-  
+  location: Location
+): void => {  
   camera.animateTo(position, location.position, 2000, controls);
-  controls.enabled=true;
-  return;
-
-  // Calculate target position for camera
-  const cameraTarget = new THREE.Vector3().copy(position);
-  
-  // If we have a location with a camera view, use it
-  if (location && location.cameraView) {
-    const { position: relativePos, distance } = location.cameraView;
-    
-    // Calculate camera position based on relative position and distance
-    const cameraPosition = new THREE.Vector3()
-      .copy(cameraTarget)
-      .add(new THREE.Vector3(
-        relativePos.x * distance,
-        relativePos.y * distance,
-        relativePos.z * distance
-      ));
-    
-    // Animate camera movement
-    new TWEEN.Tween(camera.position)
-      .to({
-        x: cameraPosition.x,
-        y: cameraPosition.y,
-        z: cameraPosition.z
-      }, 2000)
-      .easing(TWEEN.Easing.Cubic.InOut)
-      .start();
-    
-    // Animate controls target
-    new TWEEN.Tween(controls.target)
-      .to({
-        x: cameraTarget.x,
-        y: cameraTarget.y,
-        z: cameraTarget.z
-      }, 2000)
-      .easing(TWEEN.Easing.Cubic.InOut)
-      .start();
-  } else {
-    // Simple navigation without specific camera view
-    new TWEEN.Tween(camera.position)
-      .to({
-        x: position.x + 1000,
-        y: position.y + 1000,
-        z: position.z + 1000
-      }, 2000)
-      .easing(TWEEN.Easing.Cubic.InOut)
-      .start();
-    
-    new TWEEN.Tween(controls.target)
-      .to({
-        x: position.x,
-        y: position.y,
-        z: position.z
-      }, 2000)
-      .easing(TWEEN.Easing.Cubic.InOut)
-      .start();
-  }
 }; 
